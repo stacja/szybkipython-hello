481defcb3b9bb7e7057df4cc290db1552871c565
Michal Galka <galka.michal@gmail.com>
2016-10-11 08:13:52 +0200

    Added hello applications

---
+++ hello.py
@@ -0,0 +1,6 @@
+import sys
+
+if __name__ == '__main__':
+    print('It works!')
+    print(sys.version_info)
+    
---
+++ hellotk.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+master = Tk()
+text = Text(master)
+text.insert(INSERT, "It works!")
+text.pack()
+mainloop()
+
